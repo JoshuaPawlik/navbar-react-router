6a93705a1420eee9c4aa8821f90b6444d7ee6d64
Joshua Pawlik <joshuapawlik12@gmail.com>
2022-07-26 13:06:01 -0500

    Route without React-Router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import Navbar from './Navbar'
+import Pricing from './pages/Pricing'
+import About from './pages/About'
+import Home from './pages/Home'
 
 function App() {
+
+  let component
+  switch (window.location.pathname){
+    case "/":
+      component = <Home />
+      break
+    case "/pricing":
+      component = <Pricing />
+      break
+    case "/about":
+      component = <About />
+      break
+    default:
+      break
+
+  }
+
   return (
-    null
+    <>
+      <Navbar />
+      <div className="container">{component}</div>
+    </>
   );
 }
 
